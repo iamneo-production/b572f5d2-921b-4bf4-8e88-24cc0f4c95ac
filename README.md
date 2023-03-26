c275f6b841fa2d2abe84c8e8976e1884aac6c89e
amishasharma03 <as7184@srmist.edu.in>
2023-03-26 16:43:50 +0000

    Backup Branch before deleting

---
+++ README.md
@@ -0,0 +1,2 @@
+# b572f5d2-921b-4bf4-8e88-24cc0f4c95ac
+https://sonarcloud.io/summary/overall?id=examly-test_b572f5d2-921b-4bf4-8e88-24cc0f4c95ac
---
+++ python1.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask
+app = Flask(_name_)
+
+@app.route("/")
+def main():
+    return "Welcome!"
+@app.route('/I am fine')
+def hello():
+    return 'I am not good , n u'
+    if _name_=="_main_"
+        app.run()
